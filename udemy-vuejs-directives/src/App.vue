a1740177fddb331570f26192ba55c80c6d88434e
cenk.pekyaman <cenkpekyaman@yahoo.com>
2017-10-17 00:18:17 +0300

    custom directives

---
+++ udemy-vuejs-directives/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <div id="custom-directives">
+            <h1>Custom Directives</h1>
+            <p v-highlight:background.delayed="'red'">Color this</p>
+            <p v-app-highligt:background.delayed.blink="highlightData">Color this too</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                highlightData : {
+                    mainColor : 'green',
+                    secondColor : 'red',
+                    interval : 1000,
+                    delay : 2000
+                }
+            }
+        },
+        directives: {
+            appHighligt: {
+                bind(el, binding, vnode) {
+                    let delay = 0
+                    if (binding.modifiers['delayed']) {
+                        delay = binding.value.delay
+                    }
+
+                    if (binding.modifiers['blink']) {
+                        let mainColor = binding.value.mainColor
+                        let secondColor = binding.value.secondColor
+                        let currentColor = mainColor
+
+                        setTimeout(() => {
+                            setInterval(() => {
+                                currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor
+                                if (binding.arg === 'background') {
+                                    el.style.backgroundColor = currentColor
+                                } else {
+                                    el.style.color = currentColor
+                                }
+                            }, binding.value.interval)
+                        }, delay)
+
+                    } else {
+                        setTimeout(() => {
+                            if (binding.arg === 'background') {
+                                el.style.backgroundColor = binding.value.mainColor
+                            } else {
+                                el.style.color = binding.value.mainColor
+                            }
+                        }, delay)
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    div {
+        width: 60%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
